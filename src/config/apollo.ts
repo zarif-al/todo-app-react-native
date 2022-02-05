f7a1ea79bc3bb0818e09e5d182bedcb8e03589eb
zarif-al <zarif_al96@outlook.com>
2022-02-05 15:47:43 +0600

    removed env variable

--- src/config/apollo.ts
+++ src/config/apollo.ts
@@ -1,9 +1,8 @@
 import { HttpLink, ApolloClient, InMemoryCache } from '@apollo/client';
-import Config from 'react-native-config';
 import auth from '@react-native-firebase/auth';
 import { setContext } from '@apollo/client/link/context';
 
-const GRAPHQL_URI = Config.API_PRODUCTION + '/graphql';
+const GRAPHQL_URI = 'https://zarif-todo-app-api.herokuapp.com/graphql/graphql';
 
 const httpLink = new HttpLink({ uri: GRAPHQL_URI });
 
